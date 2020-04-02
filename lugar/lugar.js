fc812eb041c0a5c6fa94409cab0d45ff3126ac7c
alucardxdevil <avelardo.sjazz@outlook.com>
2020-04-02 04:29:54 -0600

    Primer commit

---
+++ lugar/lugar.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(dir) => {
+
+    const encodeUrl = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
+        timeout: 1000,
+        headers: { 'x-rapidapi-key': 'd5b72ee4cbmshfad92a6180632dep14c928jsnc9488f37f5a1' }
+    });
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para la ${dir}`);
+    }
+
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+
+
+    return {
+        direccion,
+        lat,
+        lng
+    }
+}
+
+module.exports = {
+    getLugarLatLng
+}
